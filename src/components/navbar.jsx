39523eb3d4df64808429db8a00fa98b23b179edd
Komaru <matveykostiapk@gmail.com>
2025-11-17 21:24:09 +0200

    Fix navbar project link to use hash routing

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -2,7 +2,7 @@ export default function Navbar () {
     return (
         <nav>
             <a href={"/"}>Какая то там главная...</a>
-            <a href={"/projects"}>Проекты (coming not soon)</a>
+            <a href={"/#/projects"}>Проекты (coming not soon)</a>
         </nav>
     )
 }
